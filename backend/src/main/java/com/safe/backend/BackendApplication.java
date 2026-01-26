6021d98a1009cf83cb0967fc21ff5cd853998eaf
mmmina1 <chlalsdk5743@gmail.com>
2026-01-26 17:31:02 +0900

    백, 프론트 연결 완료

--- backend/src/main/java/com/safe/backend/BackendApplication.java
+++ backend/src/main/java/com/safe/backend/BackendApplication.java
@@ -3,11 +3,10 @@ package com.safe.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.safe.backend")
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
