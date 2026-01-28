1d547ec085d1b3063e91f13f9562de5e86dc51b2
mmmina1 <chlalsdk5743@gmail.com>
2026-01-28 16:13:46 +0900

    수정 됨

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,29 +11,29 @@ import LoginPage from './pages/LoginPage.jsx';
 import SignupPage from './pages/SignupPage.jsx';
 
 // 라우트 전용
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div id="root">
       <Header />
+    
+        <Routes>
+          {/* 메인 */}
+          <Route path="/" element={<MainPage />} />
 
-      <Routes>
-        {/* 메인 */}
-        <Route path="/" element={<MainPage />} />
+          {/* 로그인 */}
+          <Route path="/login" element={<LoginPage />} />
 
-        {/* 로그인 */}
-        <Route path="/login" element={<LoginPage />} />
+          {/* 회원가입 */}
+          <Route path="/signup" element={<SignupPage />} />
 
-        {/* 회원가입 */}
-        <Route path="/signup" element={<SignupPage />} />
-
-        {/* 나중에 구현 예정인 메뉴들 */}
-        <Route path="/monitoring" element={<MainPage />} />
-        <Route path="/ai" element={<MainPage />} />
-        <Route path="/community" element={<MainPage />} />
-        <Route path="/service" element={<MainPage />} />
-      </Routes>
+          {/* 나중에 구현 예정인 메뉴들 */}
+          <Route path="/monitoring" element={<MainPage />} />
+          <Route path="/ai" element={<MainPage />} />
+          <Route path="/community" element={<MainPage />} />
+          <Route path="/service" element={<MainPage />} />
+        </Routes>
 
       <Footer />
     </div>
