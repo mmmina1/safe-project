44d5ba0086d873ef11e5d82217c3ef68e9fab8dc
mmmina1 <chlalsdk5743@gmail.com>
2026-01-28 14:22:37 +0900

    1.28z

--- frontend/src/components/main/MainHeader.jsx
+++ frontend/src/components/main/MainHeader.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
-function MainHeader() {
+export default function MainHeader({phone, setPhone, onSearch, loading}) {
   return (
-    <div>
+    <div className='main-hero'>
+        <div className='hero-innner'>
+            <h1 className='hero-title'>피싱 전화번호 검색</h1>
+            <p className='hero-subtitle'>보이스 피싱으로 의심되는 번호를 조회해보세요.</p>
+
+            <div className='search-box'>
+                <input className='search-input' value={phone} onChange={(e) =>setPhone(e.target.value)} placeholder='010-1234-1234'>
+                </input>
+                
+            </div>
+        </div>
       
     </div>
   )
 }
 
-export default MainHeader
