f69866372497d18351be25f424668642cb6b7963
mmmina1 <chlalsdk5743@gmail.com>
2026-01-28 16:43:41 +0900

    힘들어ㅋㅋ

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,11 @@ import MainPage from "./components/main/MainPage.jsx";
 import CommunityList from "./components/community/CommunityList.jsx";
 import CommunityDetail from "./components/community/CommunityDetail.jsx";
 
-// 라우트 전용
-import { Routes, Route} from 'react-router-dom';
+// 페이지 컴포넌트들
+import MainPage from './components/main/MainPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignupPage from './pages/SignupPage.jsx';
+import CommunityList from './components/community/CommunityList.jsx';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -18,22 +21,22 @@ export default function App() {
     <BrowserRouter>
       <Header />
     
-        <Routes>
-          {/* 메인 */}
-          <Route path="/" element={<MainPage />} />
-
-          {/* 로그인 */}
-          <Route path="/login" element={<LoginPage />} />
+      <main className="app-main">
+        <div className="container">
+        {/* 네비/Routes는 container 안에 */}
+        <nav className="my-3 border-bottom pb-2">
+          <Link to="/" className="me-3">🏠 홈</Link>
+          <Link to="/chatbot">🤖 AI 챗봇</Link>
+        </nav>
 
           {/* 회원가입 */}
           <Route path="/signup" element={<SignupPage />} />
 
-          {/* 나중에 구현 예정인 메뉴들 */}
-          <Route path="/monitoring" element={<MainPage />} />
-          <Route path="/ai" element={<MainPage />} />
-          <Route path="/community" element={<MainPage />} />
-          <Route path="/service" element={<MainPage />} />
-        </Routes>
+        <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/monitoring" element={<MainPage />} />
+        <Route path="/ai" element={<MainPage />} />
+        <Route path="/community" element={<CommunityList />} />
+        <Route path="/service" element={<MainPage />} />
 
       <Footer />
     </BrowserRouter>
