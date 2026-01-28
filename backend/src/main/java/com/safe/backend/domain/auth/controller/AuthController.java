03a568dac1b8d963bebfb8c182e721155a2bf08c
Kimeunho0710 <keh710427@gmail.com>
2026-01-28 11:53:21 +0900

    회원가입 오류

--- backend/src/main/java/com/safe/backend/domain/auth/controller/AuthController.java
+++ backend/src/main/java/com/safe/backend/domain/auth/controller/AuthController.java
@@ -3,13 +3,11 @@ package com.safe.backend.domain.auth.controller;
 import com.safe.backend.domain.auth.dto.SignupRequest;
 import com.safe.backend.domain.auth.service.AuthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
     private final AuthService authService;
 
---
+++ backend/src/main/java/com/safe/backend/global/error/ErrorResponse.java
@@ -0,0 +1,9 @@
+package com.safe.backend.global.error;// com.safe.backend.global.error.ErrorResponse.java
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
