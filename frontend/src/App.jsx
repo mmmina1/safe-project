4e366f0c10ea3491dabd96213079c738086d2ba1
LeeMinYeong725 <lmy3893@gmail.com>
2026-01-28 17:04:37 +0900

    git

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,6 +37,7 @@ function App() {
       style={{ 
         display: 'flex', 
         flexDirection: 'column', 
+        height: isPopup ? '100vh' : 'auto',
         minHeight: '100vh',
         overflow: isPopup ? 'auto' : 'visible'
       }}
