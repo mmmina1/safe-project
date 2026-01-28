63c95c8c80256f1d7ab410412846e940239292f1
mmmina1 <chlalsdk5743@gmail.com>
2026-01-28 15:52:57 +0900

    1/28 3:52 커밋

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import './App.css';
-import MainPage from './components/main/MainPage.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import MainPage from "./components/main/MainPage.jsx";
+
+import CommunityList from "./components/community/CommunityList.jsx";
+import CommunityDetail from "./components/community/CommunityDetail.jsx";
+
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+export default function App() {
   return (
-      <div id="root">
+    <BrowserRouter>
       <Header />
-      <MainPage/>
-      <Footer />
-    </div>
-  )
-}
 
-export default App;
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/community" element={<CommunityList />} />
+        <Route path="/community/:id" element={<CommunityDetail />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  );
+}
