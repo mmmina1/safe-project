17a9350996581c3cae4fb8b6023ad0e2629c09a6
Kimeunho0710 <keh710427@gmail.com>
2026-01-28 13:58:40 +0900

    로그인 테스트

--- backend/src/main/java/com/safe/backend/domain/auth/controller/AuthController.java
+++ backend/src/main/java/com/safe/backend/domain/auth/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.safe.backend.domain.auth.controller;
 
+import com.safe.backend.domain.auth.dto.LoginRequest;
 import com.safe.backend.domain.auth.dto.SignupRequest;
 import com.safe.backend.domain.auth.service.AuthService;
 import org.springframework.http.ResponseEntity;
@@ -29,5 +30,20 @@ public class AuthController {
         // 3. 클라이언트에게 응답
         return ResponseEntity.ok("회원가입이 완료되었습니다");
 
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+        try {
+            authService.login(
+                    request.getEmail(),
+                    request.getPassword()
+            );
+            // 나중에는 여기서 JWT 토큰 리턴하거나, 유저 정보 리턴하는 걸로 확장 가능
+            return ResponseEntity.ok("로그인에 성공했습니다");
+        } catch (IllegalArgumentException e) {
+            // 프론트에서 err.response.data 로 바로 사용 가능
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
