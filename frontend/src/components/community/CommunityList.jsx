3b5289f5a7779bec7e27794e7510762ebc387a6e
mmmina1 <chlalsdk5743@gmail.com>
2026-01-28 17:30:45 +0900

    1/28 17:30 커밋

--- frontend/src/components/community/CommunityList.jsx
+++ frontend/src/components/community/CommunityList.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
+import "../../assets/css/Community.css"
 
 function CommunityList() {
+
+  const CATEFORIES = ["전체", "긴급사칭", "공문사칭", "결제사기", "피싱", "기타"];
+
   return (
     <div>
       
---
+++ frontend/src/components/community/CommunityPost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CommunityPost() {//카드 렌더링
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CommunityPost
