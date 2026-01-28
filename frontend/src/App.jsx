42f51567378e68cdd6bafc6d6f3aa0cead404e99
Kimeunho0710 <keh710427@gmail.com>
2026-01-28 15:50:40 +0900

    로그인,회원가입

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,43 @@
+// src/App.jsx
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
-import './App.css';
+
+// 페이지 컴포넌트들
 import MainPage from './components/main/MainPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignupPage from './pages/SignupPage.jsx';
+
+// 라우트 전용
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  
   return (
-      <div id="root">
+    <div id="root">
       <Header />
-      <MainPage/>
+
+      <Routes>
+        {/* 메인 */}
+        <Route path="/" element={<MainPage />} />
+
+        {/* 로그인 */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* 회원가입 */}
+        <Route path="/signup" element={<SignupPage />} />
+
+        {/* 나중에 구현 예정인 메뉴들 */}
+        <Route path="/monitoring" element={<MainPage />} />
+        <Route path="/ai" element={<MainPage />} />
+        <Route path="/community" element={<MainPage />} />
+        <Route path="/service" element={<MainPage />} />
+      </Routes>
+
       <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
