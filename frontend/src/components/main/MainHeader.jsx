a99a867db7ff35da365d21cb4472cf23e6fa84d6
mmmina1 <chlalsdk5743@gmail.com>
2026-01-29 15:36:36 +0900

    1/29 15:36 민영아 가져가

--- frontend/src/components/main/MainHeader.jsx
+++ frontend/src/components/main/MainHeader.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
-export default function MainHeader({phone, setPhone, onSearch, loading}) {
+export default function MainHeader({ phone, setPhone, onSearch, loading }) {
   return (
-    <div className='main-hero'>
-        <div className='hero-innner'>
-            <h1 className='hero-title'>피싱 전화번호 검색</h1>
-            <p className='hero-subtitle'>보이스 피싱으로 의심되는 번호를 조회해보세요.</p>
+    <div className='main-wrap'>
+      <h1 className='main-title'>피싱 전화번호 검색</h1>
+      <p className='main-subtitle'>보이스 피싱으로 의심되는 번호를 조회해보세요.</p>
 
-            <div className='search-box'>
-                <input className='search-input' value={phone} onChange={(e) =>setPhone(e.target.value)} placeholder='010-1234-1234'>
-                </input>
-
-                <button className='search-btn' onClick={onSearch} disabled={loading}>🔍</button>
-            </div>
-        </div>
-      
+      <div className='search-box'>
+        <input
+          className='search-input'
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder='010-1234-1234'
+        />
+        <button className='search-btn' onClick={onSearch} disabled={loading}>🔍</button>
+      </div>
     </div>
   )
 }
